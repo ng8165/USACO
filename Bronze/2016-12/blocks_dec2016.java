9dd6b82bda8a822062d5e7a34dd6f680f9c395af
Nelson Gou <nel4gou@gmail.com>
2021-02-08 14:23:19 -0800

    dec2016-2

--- Bronze/2016-12/blocks_dec2016.java
+++ Bronze/2016-12/blocks_dec2016.java
@@ -1,72 +1,43 @@
 // Block Game - USACO Bronze December 2016 (http://www.usaco.org/index.php?page=viewproblem2&cpid=664)
-// This problem was completed on November 20, 2020, in 1 hour 17 minutes, with all 10/10 test cases passed (second try)
+// This problem was done as practice on Monday, October 5, 2020
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class blocks_dec2016 {
     static int numBoards;
-    static String[][] boardWords;
+    static String[][] boards;
 
-    public static int[] boards() {
-        int[] requiredLetters = new int[26];
+    public static int[] blocks() {
+        int[] neededChars = new int[26];
 
         for (int i=0; i<numBoards; i++) {
-            char[] word1 = boardWords[i][0].toCharArray();
-            char[] word2 = boardWords[i][1].toCharArray();
+            String word1 = boards[i][0];
+            char[] word1Array = word1.toCharArray();
+            int[] word1Chars = new int[26];
 
-            ArrayList<Character> uniqueLetters = determineUniqueLetters(boardWords[i][0] + boardWords[i][1]);
+            String word2 = boards[i][1];
+            char[] word2Array = word2.toCharArray();
+            int[] word2Chars = new int[26];
 
-            for (char currLetter: uniqueLetters) {
-                int word1LetterCnt = 0;
-                int word2LetterCnt = 0;
+            // turn the current words into characters
+            for (char currChar: word1Array) {
+                word1Chars[currChar - 'a']++;
+            }
+            for (char currChar: word2Array) {
+                word2Chars[currChar - 'a']++;
+            }
 
-                for (char word1Letter: word1) {
-                    if (word1Letter == currLetter) {
-                        word1LetterCnt++;
-                    }
-                }
-
-                for (char word2Letter: word2) {
-                    if (word2Letter == currLetter) {
-                        word2LetterCnt++;
-                    }
-                }
-
-                int maxLetterCnt = Math.max(word1LetterCnt, word2LetterCnt);
-
-                requiredLetters[currLetter - 'a'] += maxLetterCnt;
+            // add those characters into the master list of needed chars
+            for (int j=0; j<26; j++) {
+                neededChars[j] += Math.max(word1Chars[j], word2Chars[j]);
             }
         }
 
-        return requiredLetters;
-    }
-
-    public static ArrayList<Character> determineUniqueLetters(String combinedString) {
-        char[] combinedWord = combinedString.toCharArray();
-
-        ArrayList<Character> uniqueLetters = new ArrayList<>();
-
-        for (char letter: combinedWord) {
-            boolean found = false;
-
-            for (char uniqueLetter: uniqueLetters) {
-                if (letter == uniqueLetter) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                uniqueLetters.add(letter);
-            }
-        }
-
-        return uniqueLetters;
+        return neededChars;
     }
 
     public static void main(String[] args) throws IOException {
@@ -75,19 +46,19 @@ public class blocks_dec2016 {
         Scanner sc = new Scanner(new File(problemName + ".in"));
 
         numBoards = sc.nextInt();
-        boardWords = new String[numBoards][2];
+        boards = new String[numBoards][2];
         for (int i=0; i<numBoards; i++) {
-            boardWords[i][0] = sc.next();
-            boardWords[i][1] = sc.next();
+            boards[i][0] = sc.next();
+            boards[i][1] = sc.next();
         }
 
         // algorithm
-        int[] requiredLetters = boards();
+        int[] neededChars = blocks();
 
         // output
         PrintWriter out = new PrintWriter(new FileWriter(problemName + ".out"));
         for (int i=0; i<26; i++) {
-            out.println(requiredLetters[i]);
+            out.println(neededChars[i]);
         }
         out.close();
     }
--- Bronze/2016-12/cowsignal_dec2016.java
+++ Bronze/2016-12/cowsignal_dec2016.java
@@ -1,36 +1,34 @@
 // The Cow-Signal - USACO Bronze December 2016 (http://www.usaco.org/index.php?page=viewproblem2&cpid=665)
-// This problem was completed on November 20, 2020, in 26 minutes, with all 10/10 test cases passed (second try)
+// This problem was done as practice on Monday, October 5, 2020
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class cowsignal_dec2016 {
-    static int length;
-    static int width;
-    static int multiplier;
+    static int rows;
+    static int columns;
+    static int sizeEnlarge;
     static char[][] signal;
 
-    public static char[][] cowsignal() {
-        char[][] modifiedSignal = new char[length*multiplier][width*multiplier];
+    public static String[] cowsignal() {
+        String[] newSignal = new String[rows*sizeEnlarge];
+        Arrays.fill(newSignal, "");
 
-        for (int i=0, idx1=0; i<length; i++) {
-            char[] modifiedLine = new char[width*multiplier];
-
-            for (int j=0, idx2=0; j<width; j++) {
-                for (int k=0; k<multiplier; k++) {
-                    modifiedLine[idx2++] = signal[i][j];
+        for (int row=0; row<rows; row++) {
+            for (int i=0; i<sizeEnlarge; i++) {
+                for (int col=0; col<columns; col++) {
+                    for (int j=0; j<sizeEnlarge; j++) {
+                        newSignal[row*sizeEnlarge+i] += signal[row][col];
+                    }
                 }
-            }
-
-            for (int j=0; j<multiplier; j++) {
-                modifiedSignal[idx1++] = modifiedLine;
             }
         }
 
-        return modifiedSignal;
+        return newSignal;
     }
 
     public static void main(String[] args) throws IOException {
@@ -38,25 +36,22 @@ public class cowsignal_dec2016 {
         String problemName = "cowsignal";
         Scanner sc = new Scanner(new File(problemName + ".in"));
 
-        length = sc.nextInt();
-        width = sc.nextInt();
-        multiplier = sc.nextInt();
-        signal = new char[length][width];
+        rows = sc.nextInt();
+        columns = sc.nextInt();
+        sizeEnlarge = sc.nextInt();
+        signal = new char[rows][columns];
         sc.nextLine();
-        for (int i=0; i<length; i++) {
+        for (int i=0; i<rows; i++) {
             signal[i] = sc.nextLine().toCharArray();
         }
 
         // algorithm
-        char[][] modifiedSignal = cowsignal();
+        String[] newSignal = cowsignal();
 
         // output
         PrintWriter out = new PrintWriter(new FileWriter(problemName + ".out"));
-        for (int i=0; i<length*multiplier; i++) {
-            for (int j=0; j<width*multiplier; j++) {
-                out.print(modifiedSignal[i][j]);
-            }
-            out.println();
+        for (int i=0; i<rows*sizeEnlarge; i++) {
+            out.println(newSignal[i]);
         }
         out.close();
     }
