9273ed8a8a93d5d79ed7ee904a39565351030b15
Nelson Gou <nel4gou@gmail.com>
2021-02-08 14:35:51 -0800

    jan2019 bronzebooster

--- Bronze/2019-01/guess_jan2019.java
+++ Bronze/2019-01/guess_jan2019.java
@@ -1,6 +1,3 @@
-// Guess the Animal - USACO Bronze January 2019 (http://www.usaco.org/index.php?page=viewproblem2&cpid=893)
-// This problem was partially completed on October 25, 2020, in 1 hour and 23 minutes, with 3/10 test cases passed (first try)
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,76 +5,50 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class guess_jan2019 {
+public class guess_bronzebooster {
     static int numAnimals;
     static String[] animals;
-    static int[] numCharacteristics;
-    static ArrayList<String>[] characteristics;
+    static ArrayList<ArrayList<String>> characteristics;
 
     public static int guess() {
-        // find cow with most characteristics
-        int maxCharacteristics = 0;
-        int maxCharacterID = -1;
+        int maxCommonCharacteristics = -1;
         for (int i=0; i<numAnimals; i++) {
-            if (numCharacteristics[i] > maxCharacteristics) {
-                maxCharacterID = i;
-            }
-        }
+            for (int j=i+1; j<numAnimals; j++) {
+                // compare the two animals to find the maximum number of common characteristics
+                int currCommonCharacteristics = 0;
 
-        int[] characteristicCnt = new int[numCharacteristics[maxCharacterID]];
-        for (int i=0; i<characteristicCnt.length; i++) {
-            characteristicCnt[i] = countCharacteristics(characteristics[maxCharacterID].get(i));
-        }
-
-        int maxYes = 0;
-
-        for (int i=0; i<characteristicCnt.length; i++) {
-            if (characteristicCnt[i] > 1) {
-                maxYes++;
-            }
-        }
-
-        return maxYes+1;
-    }
-
-    public static boolean searchCharacteristics(String characteristic, ArrayList<String> uniqueCharacteristics) {
-        for (String uniqueCharacteristic: uniqueCharacteristics) {
-            if (characteristic.equals(uniqueCharacteristic)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-    public static int countCharacteristics(String characteristic) {
-        int characteristicCnt = 0;
-
-        for (int i=0; i<numAnimals; i++) {
-            for (int j=0; j<numCharacteristics[i]; j++) {
-                if (characteristics[i].get(j).equals(characteristic)) {
-                    characteristicCnt++;
+                for (int k=0; k<characteristics.get(i).size(); k++) {
+                    for (int l=0; l<characteristics.get(j).size(); l++) {
+                        if (characteristics.get(i).get(k).equals(characteristics.get(j).get(l))) {
+                            currCommonCharacteristics++;
+                        }
+                    }
                 }
+
+                maxCommonCharacteristics = Math.max(maxCommonCharacteristics, currCommonCharacteristics);
             }
         }
 
-        return characteristicCnt;
+        maxCommonCharacteristics++;
+
+        return maxCommonCharacteristics;
     }
 
     public static void main(String[] args) throws IOException {
         // input
-        String problemName = "guess";
-        Scanner sc = new Scanner(new File(problemName + ".in"));
+        Scanner sc = new Scanner(new File("guess.in"));
 
         numAnimals = sc.nextInt();
         animals = new String[numAnimals];
-        numCharacteristics = new int[numAnimals];
-        characteristics = new ArrayList[numAnimals];
+        characteristics = new ArrayList<>();
         for (int i=0; i<numAnimals; i++) {
+            characteristics.add(new ArrayList<>());
+
             animals[i] = sc.next();
-            numCharacteristics[i] = sc.nextInt();
-            characteristics[i] = new ArrayList<>();
-            for (int j=0; j<numCharacteristics[i]; j++) {
-                characteristics[i].add(sc.next());
+            int numCharacteristics = sc.nextInt();
+
+            for (int j=0; j<numCharacteristics; j++) {
+                characteristics.get(i).add(sc.next());
             }
         }
 
@@ -85,7 +56,7 @@ public class guess_jan2019 {
         int maxYes = guess();
 
         // output
-        PrintWriter out = new PrintWriter(new FileWriter(problemName + ".out"));
+        PrintWriter out = new PrintWriter(new FileWriter("guess.out"));
         out.println(maxYes);
         out.close();
     }
