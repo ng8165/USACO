3ea2f2e9a4754604ab2b23f49e954004a7a26c78
Nelson Gou <nel4gou@gmail.com>
2021-02-08 14:40:42 -0800

    open2019 bronzebooster

--- Bronze/2019-03/evolution_open2019.java
+++ Bronze/2019-03/evolution_open2019.java
@@ -1,6 +1,3 @@
-// Cow Evolution - USACO Bronze US Open 2019 (http://www.usaco.org/index.php?page=viewproblem2&cpid=940)
-// This problem was guessed on November 8, 2020, in 20 minutes, with 9/17 test cases passed (first try)
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,31 +5,102 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class evolution_open2019 {
-    static int subPopulationCnt;
-    static ArrayList<String>[] characteristics;
+public class evolution_bronzebooster {
+    static int numPopulations;
+    static ArrayList<ArrayList<String>> populationsInput = new ArrayList<>();
+
+    public static String evolution() {
+        // change the inputs into which populations each characteristic appears in
+        ArrayList<String> characteristics = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> populations = new ArrayList<>();
+        for (int i=0; i<populationsInput.size(); i++) {
+            for (int j=0; j<populationsInput.get(i).size(); j++) {
+                boolean found = false;
+
+                for (int k=0; k<characteristics.size(); k++) {
+                    if (populationsInput.get(i).get(j).equals(characteristics.get(k))) {
+                        found = true;
+                        populations.get(k).add(i);
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    characteristics.add(populationsInput.get(i).get(j));
+                    populations.add(new ArrayList<>());
+                    populations.get(populations.size()-1).add(i);
+                }
+            }
+        }
+
+        // check every pair of characteristics to see if it the evolutionary tree is valid
+        for (int i=0; i<characteristics.size(); i++) {
+            for (int j=0; j<characteristics.size(); j++) {
+                if (i==j || populations.get(i).size() == 1 || populations.get(j).size() == 1) {
+                    continue;
+                }
+
+                // search for a common population
+                if (hasCommonElement(populations.get(i), populations.get(j))) {
+                    if (hasUniqueElement(populations.get(i), populations.get(j)) && hasUniqueElement(populations.get(j), populations.get(i))) {
+                        return "no";
+                    }
+                }
+            }
+        }
+
+        return "yes";
+    }
+
+    public static boolean hasCommonElement(ArrayList<Integer> arrList1, ArrayList<Integer> arrList2) {
+        for (int int1: arrList1) {
+            for (int int2: arrList2) {
+                if (int1 == int2) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    public static boolean hasUniqueElement(ArrayList<Integer> arrList1, ArrayList<Integer> arrList2) {
+        for (int int1: arrList1) {
+            boolean found = false;
+
+            for (int int2: arrList2) {
+                if (int1 == int2) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public static void main(String[] args) throws IOException {
         // input
-        String problemName = "evolution";
-        Scanner sc = new Scanner(new File(problemName + ".in"));
+        Scanner sc = new Scanner(new File("evolution.in"));
 
-        subPopulationCnt = sc.nextInt();
-        characteristics = new ArrayList[subPopulationCnt];
-        for (int i=0; i<subPopulationCnt; i++) {
-            int characteristicsCnt = sc.nextInt();
-            characteristics[i] = new ArrayList<>();
-            for (int j=0; j<characteristicsCnt; j++) {
-                characteristics[i].add(sc.next());
+        numPopulations = sc.nextInt();
+        for (int i=0; i<numPopulations; i++) {
+            populationsInput.add(new ArrayList<>());
+            int currNumCharacteristics = sc.nextInt();
+            for (int j=0; j<currNumCharacteristics; j++) {
+                populationsInput.get(i).add(sc.next());
             }
         }
 
         // algorithm
-
+        String isProperEvolutionTree = evolution();
 
         // output
-        PrintWriter out = new PrintWriter(new FileWriter(problemName + ".out"));
-        out.println("yes");
+        PrintWriter out = new PrintWriter(new FileWriter("evolution.out"));
+        out.println(isProperEvolutionTree);
         out.close();
     }
 }
