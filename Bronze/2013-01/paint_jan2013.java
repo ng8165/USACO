2bf9c1206065d6d5aa70c0066ecdfd45663f4865
Nelson Gou <nel4gou@gmail.com>
2021-02-08 14:32:09 -0800

    jan2013

---
+++ Bronze/2013-01/paint_jan2013.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class paint_jan2013 {
+    public static void main(String[] args) throws IOException {
+        // input
+        String problemName = "paint";
+        Scanner sc = new Scanner(new File(problemName + ".in"));
+
+
+
+        // algorithm
+
+
+
+        PrintWriter out = new PrintWriter(new FileWriter(problemName + ".out"));
+        out.println();
+        out.close();
+    }
+}
