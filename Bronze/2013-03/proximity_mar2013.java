63a8d1084be54f7575f8c14fceeb67f2f0163540
Nelson Gou <nel4gou@gmail.com>
2021-02-09 12:20:22 -0800

    proximity_mar2013: Sort cows backwards to improve runtime

--- Bronze/2013-03/proximity_mar2013.java
+++ Bronze/2013-03/proximity_mar2013.java
@@ -1,3 +1,6 @@
+// Breed Proximity - USACO Bronze March 2013 (http://www.usaco.org/index.php?page=viewproblem2&cpid=260)
+// This problem completed for the USACO Silver Udemy Course, passing all 10/10 test cases.
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,7 +19,11 @@ public class proximity_mar2013 {
         }
 
         public int compareTo(Cow another) {
-            return breed - another.breed;
+            if (breed == another.breed) {
+                return idx - another.idx;
+            } else {
+                return another.breed - breed;
+            }
         }
     }
 
@@ -38,22 +45,12 @@ public class proximity_mar2013 {
         int maxBreed = -1;
         int idx = 0;
         while (idx < numCows) {
-            // identify start and end of a new section of cow breeds
-            int currBreed = cows[idx].breed;
-            int endIdx = idx+1;
-            while (endIdx < numCows && cows[endIdx].breed == cows[idx].breed) {
-                endIdx++;
+            if (cows[idx].breed == cows[idx+1].breed && (cows[idx+1].idx-cows[idx].idx) <= K) {
+                maxBreed = cows[idx].breed;
+                break;
             }
 
-            // see if these cows are "crowded"
-            for (int i=idx; i<endIdx-1; i++) {
-                if (cows[i+1].idx - cows[i].idx <= K) {
-                    maxBreed = Math.max(maxBreed, currBreed);
-                    break;
-                }
-            }
-
-            idx = endIdx;
+            idx++;
         }
 
         // output
