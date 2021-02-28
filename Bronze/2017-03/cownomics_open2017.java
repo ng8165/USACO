430fa8d1128292a548bc7a26169c1fa94f0adcac
Nelson Gou <nel4gou@gmail.com>
2021-02-27 21:37:44 -0800

    Modify cownomics_open2017.java to include a HashSet based implementation

--- Bronze/2017-03/cownomics_open2017.java
+++ Bronze/2017-03/cownomics_open2017.java
@@ -1,73 +1,74 @@
-// Bovine Genomics - USACO Bronze Open 2017 (http://www.usaco.org/index.php?page=viewproblem2&cpid=736)
-// This problem was mostly completed on Sunday, September 13, 2020, in 45 minutes, with 9/10 test cases passed (first try)
+// Bovine Genomics - USACO Bronze US Open 2017 (http://www.usaco.org/index.php?page=viewproblem2&cpid=736)
+// This problem was completed as classwork for the USACO Silver 1 Class on 2/25/21.
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
 import java.util.Scanner;
 
-public class cownomics_open2017 {
+public class L4_cownomics {
     static int numCows;
-    static int genomeLength;
-    static char[][] spottedCows;
-    static char[][] plainCows;
+    static int numGenomes;
+    static HashSet<Character>[] spotty;
+    static HashSet<Character>[] plain;
 
     public static int cownomics() {
-        int numMutations = 0;
+        int numSpottyPos = 0;
 
-        for (int pos=0; pos<genomeLength; pos++) {
-            boolean uniqueGenome = true;
+        for (int i=0; i<numGenomes; i++) {
+            boolean found = false;
 
-            for (int cowIdx=0; cowIdx<numCows; cowIdx++) {
-                if (!isUniqueGenome(pos, spottedCows[cowIdx][pos])) {
-                    uniqueGenome = false;
+            for (char genome: plain[i]) {
+                if (spotty[i].contains(genome)) {
+                    found = true;
                     break;
                 }
             }
 
-            if (uniqueGenome) {
-                numMutations++;
+            if (!found) {
+                numSpottyPos++;
             }
         }
 
-        return numMutations;
+        return numSpottyPos;
     }
 
-    public static boolean isUniqueGenome(int genomePosition, char genome) {
-        for (int i=0; i<numCows; i++) {
-            if (plainCows[i][genomePosition] == genome) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         // input
         String problemName = "cownomics";
         Scanner sc = new Scanner(new File(problemName + ".in"));
 
         numCows = sc.nextInt();
-        genomeLength = sc.nextInt();
-        sc.nextLine();
-        spottedCows = new char[numCows][genomeLength];
-        plainCows = new char[numCows][genomeLength];
+        numGenomes = sc.nextInt();
+        plain = new HashSet[numGenomes];
+        spotty = new HashSet[numGenomes];
         for (int i=0; i<numCows; i++) {
-            spottedCows[i] = sc.nextLine().toCharArray();
+            char[] genomes = sc.next().toCharArray();
+            for (int j=0; j<numGenomes; j++) {
+                if (spotty[j] == null) {
+                    spotty[j] = new HashSet<>();
+                }
+                spotty[j].add(genomes[j]);
+            }
         }
         for (int i=0; i<numCows; i++) {
-            plainCows[i] = sc.nextLine().toCharArray();
+            char[] genomes = sc.next().toCharArray();
+            for (int j=0; j<numGenomes; j++) {
+                if (plain[j] == null) {
+                    plain[j] = new HashSet<>();
+                }
+                plain[j].add(genomes[j]);
+            }
         }
 
         // algorithm
-        int numMutations = cownomics();
+        int numSpotPos = cownomics();
 
         // output
         PrintWriter out = new PrintWriter(new FileWriter(problemName + ".out"));
-        out.println(numMutations);
+        out.println(numSpotPos);
         out.close();
     }
 }
-
