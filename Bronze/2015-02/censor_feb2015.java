6b7786a52698fa1dcaf96b8060e91f5209b12d92
Nelson Gou <nel4gou@gmail.com>
2021-04-22 17:02:38 -0700

    censor_feb2015.java: StringBuilder implementation with smart use of indexOf to optimize (USACO Silver 1 Lesson 12)

--- Bronze/2015-02/censor_feb2015.java
+++ Bronze/2015-02/censor_feb2015.java
@@ -1,68 +1,35 @@
 // Censoring (Bronze) - USACO Bronze February 2015 (http://www.usaco.org/index.php?page=viewproblem2&cpid=526)
-// This problem was partially completed on November 27, 2020, in 34 minutes, with 6/15 test cases passed (first try)
-// This problem was partially completed on November 28, 2020, during review, with 9/15 test cases passed (review)
-// Attempted with String manipulation and StringBuilder, Solution requires Stack (see here: http://www.cs.ucf.edu/~dmarino/progcontests/mysols/highschool/usaco/2014/bronze/censor.java)
+// This problem was completed as classwork for the USACO Silver 1 Class on 4/22/21.
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.*;
 
-public class censor_feb2015 {
-    static String magazine;
-    static String badWord;
-
-    public static String censor() {
-        char[] magazineArr = magazine.toCharArray();
-
-        StringBuilder goodMagazine = new StringBuilder(magazineArr.length);
-        for (char letter: magazineArr) {
-            goodMagazine.append(letter);
-            System.out.println(goodMagazine.toString());
-
-            if (goodMagazine.length() > badWord.length()) {
-                int idx = goodMagazine.lastIndexOf(badWord);
-
-                if (idx != -1) {
-                    goodMagazine.delete(idx, goodMagazine.length());
-                }
-            }
-        }
-
-        return goodMagazine.toString();
-    }
-    public static String censor2() {
-        StringBuilder goodMagazine = new StringBuilder(magazine);
-
-        while (true) {
-            int idx = goodMagazine.indexOf(badWord);
-
-            if (idx == -1) {
-                break;
-            }
-            System.out.println("idx: " + idx);
-
-            goodMagazine.delete(idx, idx+badWord.length());
-        }
-
-        return goodMagazine.toString();
-    }
-
+public class L12_censor {
     public static void main(String[] args) throws IOException {
         // input
         String problemName = "censor";
-        Scanner sc = new Scanner(new File(problemName + ".in"));
+        BufferedReader br = new BufferedReader(new FileReader(problemName + ".in"));
 
-        magazine = sc.next();
-        badWord = sc.next();
+        StringBuilder str = new StringBuilder(br.readLine());
+        String substr = br.readLine();
 
         // algorithm
-        String goodMagazine = censor2();
+        int substrIdx = 0;
+
+        while (true) {
+            substrIdx = str.indexOf(substr, substrIdx-substr.length()); // when you delete you may introduce another substr write where you were
+
+            if (substrIdx > -1) {
+                str.delete(substrIdx, substrIdx+substr.length());
+            } else {
+                break;
+            }
+        }
 
         // output
-        PrintWriter out = new PrintWriter(new FileWriter(problemName + ".out"));
-        out.println(goodMagazine);
-        out.close();
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(problemName + ".out")));
+        pw.println(str.toString());
+
+        br.close();
+        pw.close();
     }
 }
