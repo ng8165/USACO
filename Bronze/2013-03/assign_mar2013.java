37aa5f29722d2c9d26a7b610cdc02f54edda1416
Nelson Gou <nel4gou@gmail.com>
2021-07-18 22:20:06 -0700

    assign_mar2013.java: Permutation Implementation

--- Bronze/2013-03/assign_mar2013.java
+++ Bronze/2013-03/assign_mar2013.java
@@ -2,50 +2,54 @@
 // This problem was completed as classwork for the USACO Silver 2 Class on 7/15/21.
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class L20_assign {
     static int numCows;
     static int numRelationships;
-    static int[][] cows;
-    static String[] relation;
+    static String[][] relation;
 
-    static final char[] breeds = {'H', 'J', 'B'};
-    static ArrayList<Character> permutation;
+    static char[] permutation;
     static int validAssignmentCnt = 0;
 
-    public static void permute(int idx) {
+    public static void permute(int currCow) {
         // base case
-        if (idx >= numCows) {
-            if (isValidAssignment(permutation)) {
-                validAssignmentCnt++;
-            }
-
+        if (currCow >= numCows) {
+            validAssignmentCnt++;
             return;
         }
 
+        // find possible breeds
+        HashSet<Character> breeds = new HashSet<>();
+        breeds.add('H');
+        breeds.add('J');
+        breeds.add('G');
+
+        for (int prevCow=0; prevCow<currCow; prevCow++) {
+            if (relation[prevCow][currCow] != null) {
+                if (relation[prevCow][currCow].equals("S")) {
+                    if (!breeds.contains(permutation[prevCow])) {
+                        // assignment is invalid
+                        return;
+                    }
+
+                    // same relation
+                    breeds.clear();
+                    breeds.add(permutation[prevCow]);
+                } else {
+                    // different relation
+                    breeds.remove(permutation[prevCow]);
+                }
+            } // else: no relation
+        }
+
+        // do recursion for the possible breeds
         for (char breed: breeds) {
-            permutation.add(breed);
-
-            permute(idx+1);
-
-            // backtracking
-            permutation.remove(permutation.size()-1);
+            permutation[currCow] = breed;
+            permute(currCow+1);
+            permutation[currCow] = 0; // backtracking (not necessary, will be overwritten)
         }
-    }
-
-    public static boolean isValidAssignment(ArrayList<Character> assignment) {
-        for (int i=0; i<relation.length; i++) {
-            if (relation[i].equals("S") && assignment.get(cows[i][0]-1) != assignment.get(cows[i][1]-1)) {
-                return false;
-            } else if (relation[i].equals("D") && assignment.get(cows[i][0]-1) == assignment.get(cows[i][1]-1)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
     public static void main(String[] args) throws IOException {
@@ -53,32 +57,48 @@ public class L20_assign {
         String problemName = "assign";
         BufferedReader br = new BufferedReader(new FileReader(problemName + ".in"));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(problemName + ".out")));
 
         numCows = Integer.parseInt(st.nextToken());
         numRelationships = Integer.parseInt(st.nextToken());
-        permutation = new ArrayList<>();
-        permutation.add('H');
-        HashSet<String> uniqueRelationships = new HashSet<>();
+
+        /*
+        relationships saved in 2D array, indexes are cow numbers, String contents determine relationship
+        relation[i][j]: S indicates cow i and cow j are same
+                        D indicates cow i and cow j are different
+                        null indicates cow i and cow j have no relation
+        */
+        relation = new String[numCows][numCows];
         for (int i=0; i<numRelationships; i++) {
-            uniqueRelationships.add(br.readLine());
-        }
-        cows = new int[uniqueRelationships.size()][2];
-        relation = new String[uniqueRelationships.size()];
-        int idx=0;
-        for (String r: uniqueRelationships) {
-            st = new StringTokenizer(r);
-            relation[idx] = st.nextToken();
-            cows[idx][0] = Integer.parseInt(st.nextToken());
-            cows[idx++][1] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+
+            String r = st.nextToken();
+            int cowTemp1 = Integer.parseInt(st.nextToken())-1;
+            int cowTemp2 = Integer.parseInt(st.nextToken())-1;
+            int cow1 = Math.min(cowTemp1, cowTemp2);
+            int cow2 = Math.max(cowTemp1, cowTemp2);
+
+            if (relation[cow1][cow2] != null && !relation[cow1][cow2].equals(r)) {
+                // FJ gave contradictory information
+                pw.println(0);
+
+                br.close();
+                pw.close();
+                return;
+            } else {
+                relation[cow1][cow2] = r;
+            }
         }
 
         // algorithm
+        permutation = new char[numCows];
+        permutation[0] = 'H';
+
         permute(1);
+
         validAssignmentCnt *= 3;
 
         // output
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(problemName + ".out")));
-
         pw.println(validAssignmentCnt);
 
         br.close();
