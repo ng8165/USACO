dd2cf50ac26b1a76c622875febde36c58e5ba1aa
Nelson Gou <nel4gou@gmail.com>
2022-02-19 16:51:04 -0800

    Update Kattis Birthday Party: DFS with Adjacency Matrix

--- Silver/Kattis/BirthdayParty.cpp
+++ Silver/Kattis/BirthdayParty.cpp
@@ -8,16 +8,40 @@ using namespace std;
 int p, c;
 int a[5001], b[5001];
 
-set<int> adjList[101];
+bool adj[101][101];
 bool visited[101];
 
 void dfs(int person) {
     visited[person] = true;
     
-    for (int fr: adjList[person]) {
-        if (!visited[fr])
-            dfs(fr);
+    for (int i=0; i<p; i++) {
+        if (adj[person][i] && !visited[i])
+            dfs(i);
     }
+}
+
+void solve() {
+    for (int i=1; i<=c; i++) {
+        // disconnect two friends
+        adj[a[i]][b[i]] = adj[b[i]][a[i]] = false;
+
+        // perform DFS
+        memset(visited, false, sizeof(visited));
+        dfs(0);
+
+        // if any friend is not visited, then someone misses his party
+        for (int i=0; i<p; i++) {
+            if (!visited[i]) {
+                cout << "Yes\n";
+                return;
+            }
+        }
+
+        // reconnect two friends
+        adj[a[i]][b[i]] = adj[b[i]][a[i]] = true;
+    }
+
+    cout << "No\n";
 }
 
 int main() {
@@ -28,38 +52,13 @@ int main() {
         cin >> p >> c;
         if (p == 0 && c == 0) break;
 
+        memset(adj, false, sizeof(adj));
+
         for (int i=1; i<=c; i++) {
             cin >> a[i] >> b[i];
-            adjList[a[i]].insert(b[i]);
-            adjList[b[i]].insert(a[i]);
+            adj[a[i]][b[i]] = adj[b[i]][a[i]] = true;
         }
 
-        bool stop = false;
-
-        for (int i=1; i<=c; i++) {
-            adjList[a[i]].erase(b[i]);
-            adjList[b[i]].erase(a[i]);
-
-            memset(visited, false, sizeof(visited));
-            dfs(0);
-
-            for (int i=0; i<p; i++) {
-                if (!visited[i]) {
-                    cout << "Yes\n";
-                    stop = true;
-                    break;
-                }
-            }
-
-            if (stop) break;
-
-            adjList[a[i]].insert(b[i]);
-            adjList[b[i]].insert(a[i]);
-        }
-
-        if (!stop) cout << "No\n";
-
-        for (int i=0; i<p; i++)
-            adjList[i].clear();
+        solve();
     }
 }
